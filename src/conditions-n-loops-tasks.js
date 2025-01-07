c5c85a4811a3e2ca93271ca13222ee5ba113fc50
Daniil Biver <d.biver@sfbtech.org>
2025-01-07 14:58:55 +0300

    feat: add canQueenCaptureKing

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -61,8 +61,13 @@ function getMaxNumber(a, b, c) {
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  */
-function canQueenCaptureKing(/* queen, king */) {
-  throw new Error('Not implemented');
+function canQueenCaptureKing(queen, king) {
+  return (
+    queen.x === king.x ||
+    queen.y === king.y ||
+    queen.x - queen.y === king.x - king.y ||
+    queen.x + queen.y === king.x + king.y
+  );
 }
 
 /**
