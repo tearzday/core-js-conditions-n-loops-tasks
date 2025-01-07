2469e92f895634aa3b11f843cf6c3cae5430eccc
Daniil Biver <d.biver@sfbtech.org>
2025-01-07 14:04:34 +0300

    feat: add isPalindrome

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -143,8 +143,14 @@ function convertNumberToString(/* numberStr */) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let strReverse = '';
+
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    strReverse += str[i];
+  }
+
+  return str === strReverse;
 }
 
 /**
